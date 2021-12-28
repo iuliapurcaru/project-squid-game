1206efcfb188baa395d327b4c3376b48dc4a43cf
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2021-12-28 21:48:43 +0200

    Added Comtestant and Worker classes

--- Squid Game/Squid Game.cpp
+++ Squid Game/Squid Game.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 class Participant
 {
 public:
@@ -11,7 +13,17 @@ public:
     int weight;
 };
 
-using namespace std;
+class Contestant : public Participant
+{
+public:
+    int number;
+};
+
+class Worker : public Participant
+{
+public:
+    string mask;
+};
 
 int main()
 {
