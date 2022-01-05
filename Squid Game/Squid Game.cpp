c96d96863f98398b64a245e6f86f885175debc81
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2022-01-06 00:51:43 +0200

    contestants improved

--- Squid Game/Squid Game.cpp
+++ Squid Game/Squid Game.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <time.h> 
 
 using namespace std;
+
+string random_names[12] = { "James", "Robert", "Sarah", "Oliver", "Emily", "Kevin", "Stephanie", "Anna", "Sebastian", "Emma", "George", "Chloe"};
+string random_surnames[14] = { "Smith", "Johnson", "Williams", "Brown", "Jones", "Russell", "Davis", "Wilson", "Anderson", "Taylor", "Moore", "Jackson", "Thompson", "Clark" };
 
 class Participant
 {
@@ -27,5 +32,29 @@ public:
 
 int main()
 {
-    cout << "Hello World!\n";
+    int i, rand1, rand2;
+
+    int n_part = 108;
+    int n_cont = 99;
+    int n_work = 9;
+
+    Contestant contestants[99];
+    
+    srand(time(NULL));
+    for (i = 0; i < 99; i++)
+    {
+        rand1 = rand() % 12;
+        contestants[i].name = random_names[rand1];
+        rand2 = rand() % 14;
+        contestants[i].surname = random_surnames[rand2];
+        contestants[i].number = i + 1;
+    }
+
+
+
+    for (i = 0; i < 99; i++)
+    {
+        cout << contestants[i].name << " " << contestants[i].surname << " " << contestants[i].number << endl;
+    }
+
 }
