db3f2d2b41035b44abc3a0b68313edc61f7f328f
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2022-01-06 01:26:51 +0200

    improved contestants

--- Squid Game/Squid Game.cpp
+++ Squid Game/Squid Game.cpp
@@ -5,8 +5,9 @@
 
 using namespace std;
 
-string random_names[12] = { "James", "Robert", "Sarah", "Oliver", "Emily", "Kevin", "Stephanie", "Anna", "Sebastian", "Emma", "George", "Chloe"};
-string random_surnames[14] = { "Smith", "Johnson", "Williams", "Brown", "Jones", "Russell", "Davis", "Wilson", "Anderson", "Taylor", "Moore", "Jackson", "Thompson", "Clark" };
+string random_names[15] = {"Julia", "Michael", "James", "Robert", "Sarah", "Oliver", "Emily", "Kevin", "Sophie", "Anna", "Noah", "Emma", "George", "Chloe", "Lewis"};
+string random_surnames[15] = { "Hill", "Smith", "Johnson", "William", "Brown", "Jones", "Russell", "Davis", "Wilson", "Watson", "Taylor", "Moore", "Jackson", "Miller", "Clark" };
+string random_cities[10] = { "Berlin", "Rome", "Athens", "Paris", "Vienna", "London", "Dublin", "Madrid", "Oslo", "Prague" };
 
 class Participant
 {
@@ -32,7 +33,8 @@ public:
 
 int main()
 {
-    int i, rand1, rand2;
+    int i;
+    int random;
 
     int n_part = 108;
     int n_cont = 99;
@@ -43,18 +45,29 @@ int main()
     srand(time(NULL));
     for (i = 0; i < 99; i++)
     {
-        rand1 = rand() % 12;
-        contestants[i].name = random_names[rand1];
-        rand2 = rand() % 14;
-        contestants[i].surname = random_surnames[rand2];
         contestants[i].number = i + 1;
+
+        random = rand() % 15;
+        contestants[i].name = random_names[random];
+
+        random = rand() % 15;
+        contestants[i].surname = random_surnames[random];
+
+        random = rand() % 10;
+        contestants[i].city = random_cities[random];
+
+        contestants[i].debt = rand() % 100000 + 10000;
+        contestants[i].weight = rand() % 50 + 50;
     }
 
-
+    cout << "\t\t--CONTESTANTS--" << endl;
+    cout << "Number\tName\tSurname\tCity\tDebt\tWeight" << endl;
 
     for (i = 0; i < 99; i++)
     {
-        cout << contestants[i].name << " " << contestants[i].surname << " " << contestants[i].number << endl;
+        cout << contestants[i].number << "\t" << contestants[i].name << "\t" << contestants[i].surname << "\t" << contestants[i].city << "\t$" << contestants[i].debt << "\t" << contestants[i].weight << "kg " << endl;
     }
+
+    cout << "--------------------------------------------" << endl;
 
 }
