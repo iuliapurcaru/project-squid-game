3bcf508a694bee91f303d102c2363dd6a55f0612
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2022-01-06 02:21:45 +0200

    split contestants

--- Squid Game/Squid Game.cpp
+++ Squid Game/Squid Game.cpp
@@ -99,6 +99,30 @@ int main()
         cout << workers[i].mask << " " << workers[i].name << "\t" << workers[i].surname << "\t" << workers[i].city << "\t$" << workers[i].debt << "\t" << workers[i].weight << "kg " << endl;
     }
 
+    int random_contestants[99] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+        21, 22, 23, 24, 25,26, 27, 28, 29, 30, 
+        31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+        51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
+        61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
+        71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+        81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+        91, 92, 93, 94, 95, 96, 97, 98 };
+    random_shuffle(begin(random_contestants), end(random_contestants));
+    cout << "--------------------------------------------" << endl;
+
+    Contestant team_1[11];
+    cout << "Worker 1:" << endl;
+    cout << workers[0].mask << " " << workers[0].name << "\t" << workers[0].surname << "\t" << workers[0].city << "\t$" << workers[0].debt << "\t" << workers[0].weight << "kg " << endl;
+
+    for (i = 0; i < 99; i++)
+    {
+        team_1[i] = contestants[random_contestants[i]];
+        cout << team_1[i].number << "\t" << team_1[i].name << "\t" << team_1[i].surname << "\t" << team_1[i].city << "\t$" << team_1[i].debt << "\t" << team_1[i].weight << "kg " << endl;
+    }
+
+
     cout << "--------------------------------------------" << endl;
 
 }
