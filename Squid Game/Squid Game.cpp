ee4ecec0d7130afcad9a4546fa3c5bb2f0c21170
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2022-01-06 15:35:34 +0200

    added red light green light

--- Squid Game/Squid Game.cpp
+++ Squid Game/Squid Game.cpp
@@ -12,7 +12,7 @@ string random_names[15] = { "Julia", "Sam", "James", "Robert", "Sarah", "Oliver"
 string random_surnames[15] = { "Hill", "Smith", "Johnson", "William", "Brown", "Jones", "Russell", "Davis", "Wilson", "Watson", "Taylor", "Moore", "Jackson", "Miller", "Clark" };
 string random_cities[10] = { "Berlin", "Rome", "Athens", "Paris", "Vienna", "London", "Dublin", "Madrid", "Oslo", "Prague" };
 
-int random_contestants[99] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+int random_numbers[99] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 		11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 		21, 22, 23, 24, 25,26, 27, 28, 29, 30,
 		31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
@@ -31,15 +31,46 @@ public:
 	string city;
 	int debt;
 	int weight;
+
 };
 
 class Contestant : public Participant
 {
 public:
 	int number;
+
+	Contestant();
+	~Contestant() {}
 };
 
-class Worker : public Participant
+Contestant::Contestant()
+{
+	int i, random;
+
+	random = rand() % 15;
+	name = random_names[random];
+
+	random = rand() % 15;
+	surname = random_surnames[random];
+
+	random = rand() % 10;
+	city = random_cities[random];
+
+	debt = rand() % 100000 + 10000;
+	weight = rand() % 50 + 50;
+
+}
+
+//Contestant::~Contestant()
+//{
+//	delete& name;
+//	delete& surname;
+//	delete& city;
+//	delete& debt;
+//	delete& weight;
+//}
+
+class Guard : public Participant
 {
 private:
 	int team[11];
@@ -52,16 +83,16 @@ public:
 		int i = 0;
 		int jmax = j + 11;
 
-		for ( ; j < jmax; j++)
+		for (; j < jmax; j++)
 		{
-			this->team[i] = random_contestants[j];
+			this->team[i] = random_numbers[j];
 			i++;
 		}
 	}
 
 	int getTeam(int j)
 	{
-		return team[j];
+		return this->team[j];
 	}
 
 };
@@ -72,95 +103,89 @@ int main()
 	int random;
 	int shuffle[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-	Contestant contestants[99];
-
 	srand(time(NULL));
-	for (i = 0; i < 99; i++)
-	{
-		contestants[i].number = i + 1;
 
-		random = rand() % 15;
-		contestants[i].name = random_names[random];
-
-		random = rand() % 15;
-		contestants[i].surname = random_surnames[random];
-
-		random = rand() % 10;
-		contestants[i].city = random_cities[random];
-
-		contestants[i].debt = rand() % 100000 + 10000;
-		contestants[i].weight = rand() % 50 + 50;
-	}
+	Contestant contestants[99];
 
 	cout << "\t\t--CONTESTANTS--" << endl;
 	cout << "Number\tName\tSurname\tCity\tDebt\tWeight" << endl;
 
 	for (i = 0; i < 99; i++)
 	{
+		contestants[i].number = i + 1;
 		cout << contestants[i].number << "\t" << contestants[i].name << "\t" << contestants[i].surname << "\t" << contestants[i].city << "\t$" << contestants[i].debt << "\t" << contestants[i].weight << "kg " << endl;
 	}
 
 	cout << "--------------------------------------------" << endl;
 
-	Worker workers[9];
+	Guard guards[9];
 
 	string random_masks[9] = { "square", "square", "square", "circle", "circle", "circle", "triangle", "triangle", "triangle" };
 	random_shuffle(begin(random_masks), end(random_masks));
 
 	for (i = 0; i < 9; i++)
 	{
-		workers[i].mask = random_masks[i];
+		guards[i].mask = random_masks[i];
 
 		random = rand() % 15;
-		workers[i].name = random_names[random];
+		guards[i].name = random_names[random];
 
 		random = rand() % 15;
-		workers[i].surname = random_surnames[random];
+		guards[i].surname = random_surnames[random];
 
 		random = rand() % 10;
-		workers[i].city = random_cities[random];
+		guards[i].city = random_cities[random];
 
-		workers[i].debt = rand() % 100000 + 10000;
-		workers[i].weight = rand() % 50 + 50;
+		guards[i].debt = rand() % 100000 + 10000;
+		guards[i].weight = rand() % 50 + 50;
 	}
 
-	cout << "\t\t--WORKERS--" << endl;
+	cout << "\t\t--GUARDS--" << endl;
 	cout << "Mask\tName\tSurname\tCity\tDebt\tWeight" << endl;
 
 	for (i = 0; i < 9; i++)
 	{
-		cout << workers[i].mask << " " << workers[i].name << "\t" << workers[i].surname << "\t" << workers[i].city << "\t$" << workers[i].debt << "\t" << workers[i].weight << "kg " << endl;
+		cout << guards[i].mask << " " << guards[i].name << "\t" << guards[i].surname << "\t" << guards[i].city << "\t$" << guards[i].debt << "\t" << guards[i].weight << "kg " << endl;
 	}
 
 	cout << "--------------------------------------------" << endl;
 
-	random_shuffle(begin(random_contestants), end(random_contestants));
+	random_shuffle(begin(random_numbers), end(random_numbers));
 
-	cout << "--Workers and their contestants--\n";
+	cout << "--Guards and the contestants they supervise--\n";
 
 	j = 0;
 	for (i = 0; i < 9; i++)
 	{
-		workers[i].setTeam(j);
+		guards[i].setTeam(j);
 		j += 11;
 	}
 
-	for(i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++)
 	{
-		cout << "Worker " << i + 1 << ": ";
+		cout << "Guard " << i + 1 << ": ";
 		for (j = 0; j < 11; j++)
 		{
-			cout << workers[i].getTeam(j) << " ";
+			cout << guards[i].getTeam(j) << " ";
 		}
 		cout << endl;
 	}
 
 	cout << "--------------------------------------------" << endl;
 
-	cout << "Press any key to continue . . .";
+	cout << "Press enter to continue . . .";
 	getchar();
 
 	cout << endl;
-	cout << "The first game is Red Light, Green Light";
+	cout << "The first game is Red Light, Green Light" << endl;
+
+	
+	for (i = 0; i < 99; i++)
+	{
+		if (contestants[i].number % 2)
+		{
+			cout << contestants[i].number << "\t" << contestants[i].name << "\t" << contestants[i].surname << "\t" << contestants[i].city << "\t$" << contestants[i].debt << "\t" << contestants[i].weight << "kg " << endl;
+		}
+	}
 
 }
